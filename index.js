80f46fcff9ac8018991dc34b7fbdfb36f09d2785
Chris Kwan <chriswkwan@gmail.com>
2016-05-02 22:34:56 -0400

    Add another example route

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
 	res.send('Hello World!');
 });
 
+app.get('/cat', function (req, res) {
+	res.send('Meow');
+});
+
 app.listen(3000, function () {
 	console.log('Example app listening on port 3000!');
 });
